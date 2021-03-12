1689fb3d305567613b2aea7f7a34a988b993fd1e
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:33:00 -0500

    set up relationships for friendrequests

--- app/models/friendrequest.rb
+++ app/models/friendrequest.rb
@@ -1,4 +1,12 @@
 class Friendrequest < ApplicationRecord
-  belongs_to :requestor
-  belongs_to :pending_friend
+  belongs_to :requestor, class_name: 'User'
+  belongs_to :pending_friend, class_name: 'User'
+
+  def accept()
+    self.update(accepted: true)
+    if self.accepted
+        self.requestor.friends.push(self.pending_friend)
+        self.destroy
+    end
+end
 end
