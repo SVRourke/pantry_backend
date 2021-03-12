9e3515720e4b63adbf36639420261047cfbfa3ba
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:33:38 -0500

    set up relationships for list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ApplicationRecord
+    has_many :contributions
+    has_many :contributors, through: :contributions, source :user
+
+    has_many :items
+
+    has_many :list_invites
+    has_many :invited_contributors, through: :list_invites, source: :pending_contributor 
 end
