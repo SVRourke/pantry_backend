af8e8ec8c126912a2f3af0362cc36d1c559eb536
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:33:25 -0500

    set up relationships for friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,23 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend
+  belongs_to :friend, class_name: 'User'
+
+  after_create :reciprocate_friendship
+  after_destroy :remove_reciprocal
+
+  private
+
+    def reciprocate_friendship
+        if !Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+            Friendship.create(user_id: self.friend_id, friend_id: self.user_id)
+        end
+    end
+
+    def remove_reciprocal
+        opposite_friendship = Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+
+        if opposite_friendship
+            opposite_friendship.destroy        
+        end
+    end
 end
