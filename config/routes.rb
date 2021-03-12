f606b2873bd9a9aebc4aff1aacb52643b105f494
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:09:28 -0500

    resource generator for Contribution

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contributions
   resources :lists
   resources :friendrequests
   resources :friendships
---
+++ db/migrate/20210312020842_create_contributions.rb
@@ -0,0 +1,10 @@
+class CreateContributions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contributions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :list, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
