fb1f28fa07cfcebd9ac115db50c511cf83998a4e
SVRourke <samuelvanrourke@gmail.com>
2021-03-12 14:14:38 -0500

    added devise to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable, :validatable
+
     has_many :friendships, dependent: :destroy
     has_many :friends, through: :friendships
-
     # sent friend requests
     has_many :sent_requests, foreign_key: :requestor_id, class_name: 'Friendrequest'
     # requested users, new request created by pushing user onto this list
     has_many :pending_friends, through: :sent_requests
-    
     # recieved friend requests 
     has_many :requests, foreign_key: :pending_friend_id, class_name: 'Friendrequest'
     # users requesting friendship
     has_many :requestors, through: :requests
-
     # recieved list_invites
     has_many :list_invites, foreign_key: :pending_contributor_id, class_name: 'ListInvite'
-
     has_many :contributions
     has_many :lists, through: :contributions, source: :list 
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :list_invites
   resources :items
   resources :contributions
