be1906d96040c1131e28ee732339479f44511c21
SVRourke <samuelvanrourke@gmail.com>
2021-03-12 13:17:15 -0500

    added strong params and testing post action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,18 @@ class ListsController < ApplicationController
         list = List.all
         render json: list, include: [:contributors, :items]
     end
+
     # TODO: Create
+    def create
+        puts new_list_params
+
+    end
     # TODO: Read
     # TODO: Delete
+
+    private
+
+    def new_list_params
+        params.require(:list_info).permit(:name)
+    end
 end
