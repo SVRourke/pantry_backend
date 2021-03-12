ed61977b39798449f0e42cc0c9bd2c2664b562f8
SVRourke <samuelvanrourke@gmail.com>
2021-03-12 12:00:52 -0500

    added basic index action to lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,9 @@
 class ListsController < ApplicationController
     # TODO: Index
+    def index
+        list = List.all
+        render json: list, include: [:contributors, :items]
+    end
     # TODO: Create
     # TODO: Read
     # TODO: Delete
