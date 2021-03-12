9bbfb9c5e55f1f4c6dc0d0fde7dc164a3f407e71
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:32:45 -0500

    added relationships for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+    has_many :friendships, dependent: :destroy
+    has_many :friends, through: :friendships
+
+    # sent friend requests
+    has_many :sent_requests, foreign_key: :requestor_id, class_name: 'Friendrequest'
+    # requested users, new request created by pushing user onto this list
+    has_many :pending_friends, through: :sent_requests
+    
+    # recieved friend requests 
+    has_many :requests, foreign_key: :pending_friend_id, class_name: 'Friendrequest'
+    # users requesting friendship
+    has_many :requestors, through: :requests
+
+    # recieved list invites
+    has_many :list_invites, foreign_key: :pending_contributor_id, class_name: 'Listinvite'
+
+    has_many :contributions
+    has_many :lists, through: :contributions, source: :list 
+
+
 end
