c99325e5e0140e55f341df22ee3f5a6ee1a5f62d
SVRourke <samuelvanrourke@gmail.com>
2021-03-11 21:34:28 -0500

    added default false for accepted column

--- db/migrate/20210312021328_create_list_invites.rb
+++ db/migrate/20210312021328_create_list_invites.rb
@@ -1,10 +1,10 @@
 class CreateListInvites < ActiveRecord::Migration[6.1]
   def change
     create_table :list_invites do |t|
-      t.references :list, null: false, foreign_key: true
-      t.references :sender, null: false, foreign_key: true
-      t.references :pending_contributor, null: false, foreign_key: true
-      t.boolean :accepted
+      t.references :list, null: false
+      t.references :sender, null: false
+      t.references :pending_contributor, null: false
+      t.boolean :accepted, default: false
 
       t.timestamps
     end
