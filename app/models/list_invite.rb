4ef4f93b70ae8be9f4aaec970d683e84bd551e84
SVRourke <samuelvanrourke@gmail.com>
2021-03-12 08:36:04 -0500

    fixed listinvite relations and added accept method

--- app/models/list_invite.rb
+++ app/models/list_invite.rb
@@ -1,5 +1,12 @@
 class ListInvite < ApplicationRecord
-  belongs_to :list
-  belongs_to :sender
-  belongs_to :pending_contributor
+  belongs_to :requestor, class_name: 'User'
+  belongs_to :pending_contributor, class_name: 'User'
+  belongs_to :list, class_name: 'List'
+
+  def accept
+    if !Contribution.where(user_id: self.pending_contributor_id, list_id: self.list_id).any?
+      self.list.contributors.push(self.pending_contributor)
+      self.destroy
+    end
+  end
 end
