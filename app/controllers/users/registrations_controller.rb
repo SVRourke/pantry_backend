4fb9812185032965c9f6ae0ba898c0ce482b6a4a
SVRourke <samuelvanrourke@gmail.com>
2021-03-13 07:38:11 -0500

    devise registration and sessions controller from tutorial, need to customize

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+# TODO: CUSTOMIZE
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    super
+  end
+
+  private 
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up sucessfully.'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
+  
+end
