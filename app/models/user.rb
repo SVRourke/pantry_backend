f62796b963e99857b1ea0f5a3d4a63d988c848b1
SVRourke <samuelvanrourke@gmail.com>
2021-03-13 07:38:48 -0500

    added jwt stuff to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     include Devise::JWT::RevocationStrategies::JTIMatcher  
   
-    devise :database_authenticatable, :registerable, :validatable
+    devise :database_authenticatable, :registerable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
     has_many :friendships, dependent: :destroy
     has_many :friends, through: :friendships
