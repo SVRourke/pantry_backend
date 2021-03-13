07317a864110084f327ecd13ce2c50036bce15fa
SVRourke <samuelvanrourke@gmail.com>
2021-03-13 07:06:29 -0500

    added devise routes, commented out resources until devise + jwt works

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :list_invites
-  resources :items
-  resources :contributions
-  resources :lists
-  resources :friendrequests
-  resources :friendships
-  resources :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  # resources :list_invites
+  # resources :items
+  # resources :contributions
+  # resources :lists
+  # resources :friendrequests
+  # resources :friendships
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
