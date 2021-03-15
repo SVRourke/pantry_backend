b1c33b18fdcf82f0698f13c4ca23075d0e5bea7d
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 11:32:30 -0400

    added encode_token function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    before_action :authorized 
+
+    def encode_token(payload)
+        # TODO: CHANGE TO AN ENV REF IMMEDIATELY
+        JWT.encode(payload, 'MYSECRET')
+    end
 end
