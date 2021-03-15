072b42813ad91353d778e527a87b4503726bfaf1
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 08:48:48 -0400

    added create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def create
+        puts ''
+        puts user_params
+        puts ''
+        puts user_params
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+            render json: {
+                error: 'failed to sign up',
+                data: @user.errors.full_messages.to_sentence
+            }, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:password, :email, :name)
+    end
+
 end
