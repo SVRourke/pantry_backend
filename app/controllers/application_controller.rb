2d61ae75570dd66ff6e06e2c6072e0d0602528d0
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 11:33:25 -0400

    added helpers for decoding JWT, accessing a request's auth header, deriving the current user from the JWT as well as login state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,32 @@ class ApplicationController < ActionController::API
         # TODO: CHANGE TO AN ENV REF IMMEDIATELY
         JWT.encode(payload, 'MYSECRET')
     end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token(token)
+        if auth_header
+            token = auth_header.split(' ')[1]
+
+            begin
+                # TODO: CHANGE TO AN ENV REF IMMEDIATELY
+                JWT.decode(token, 'MYSECRET')[0]
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
