a9d58bb87fbdf4799887d864e61e2ab20189c17c
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 09:28:56 -0400

    added show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
+
     def create
-        puts ''
-        puts user_params
-        puts ''
-        puts user_params
         @user = User.create(user_params)
         if @user.valid?
             render json: { user: UserSerializer.new(@user) }, status: :created
@@ -15,9 +12,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        user = User.find(params[:id])
+
+        if user.valid?
+            render json: { user: UserSerializer.new(user) }, status: :created
+        else
+            render json: {
+                error: 'Not Found',
+            }, status: :not_acceptable
+        end
+    end
+
     private
     def user_params
         params.require(:user).permit(:password, :email, :name)
     end
-
+    
 end
