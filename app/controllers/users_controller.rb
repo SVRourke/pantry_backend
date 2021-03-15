1bfc172095b116b6e9ec09d604418dc02a094021
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 12:35:12 -0400

    added token on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
         @user = User.create(user_params)
+
         if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
+            @token = encode_token(user_id: @user.id)
+            render json: { 
+                user: UserSerializer.new(@user), 
+                jwt: @token 
+            }, 
+            status: :created
         else
             render json: {
                 error: 'failed to sign up',
-                data: @user.errors.full_messages.to_sentence
+                data: @user.errors
             }, status: :not_acceptable
         end
     end
