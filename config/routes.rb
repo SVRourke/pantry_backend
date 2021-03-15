e22481ae4e48c8543c055bd5ce5cd6c36b17db69
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 09:29:06 -0400

    added login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :list_invites
-  resources :items
-  resources :contributions
-  resources :lists
-  resources :friendrequests
-  resources :friendships
-  resources :users
+  # resources :list_invites
+  
+  # resources :items
+  # resources :contributions
+  # resources :lists
+  # resources :friendrequests
+  # resources :friendships
+  # resources :users
+  resources :users, only: [:create, :show]
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
