c0d6c04600e00f6059e9cb0de2f0c15bf853e754
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 15:45:37 -0400

    added subserializer FriendshipSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :email, :name
-  has_many :friends
-  has_many :requestors
+  
+  has_many :requests
+  has_many :list_invites
   has_many :lists
+  has_many :friendships
+
+  class FriendshipSerializer < ActiveModel::Serializer
+    attributes :friend_id, :created_at
+  end
 
 end
