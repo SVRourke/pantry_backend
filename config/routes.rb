0b36dac2e578899c8093d2426bd2171238baccf8
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 11:36:35 -0400

    Aadded Todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # resources :list_invites
-  
+
   # resources :items
   # resources :contributions
   # resources :lists
@@ -11,3 +11,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# TODO: MOVE SIGN UP ACTION TO REGISTRATIONS CONTROLLER
