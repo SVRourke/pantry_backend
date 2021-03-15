0b6df426ba088f826d14639a14c28e39d2dd9620
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 16:33:18 -0400

    added subserializers TODO: move to separate files

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,36 @@
 class UserSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-
-  attributes :id, :email, :name
   
-  has_many :requests
-  has_many :list_invites
-  has_many :lists
-  has_many :friendships
+  class FriendsSlice < ActiveModel::Serializer
+    attributes :id, :name, :friends_since
+    def friends_since
+      Friendship.find_by(friend_id: object.id).created_at
+    end
+  end
+  
+  class FriendRequestSerializer < ActiveModel::Serializer
+    attributes :id, :requestor_id, :requestor_name
+    def requestor_name
+      User.find(object.requestor_id).name
+    end
+  end
+  
+  class PendingFriendSerializer < ActiveModel::Serializer
+    attributes :id, :pending_name
 
-  class FriendshipSerializer < ActiveModel::Serializer
-    attributes :friend_id, :created_at
+    def pending_name
+      User.find(object.requestor_id).name
+    end
   end
 
+  attributes :id, :email, :name
+
+  has_many :friends, serializer: FriendsSlice
+  has_many :pending_friends, serializer: PendingFriendSerializer
+  has_many :requests, key: :friend_requests, serializer: FriendRequestSerializer
+  has_many :list_invites
+  has_many :lists
+
+
+
 end
+
