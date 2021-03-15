3afed54f5ac1dd54d5726633b0ec477448f821a0
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 12:49:52 -0400

    created auth controller, added create action to log a user in and respond with JWT token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,11 +1,32 @@
 class AuthController < ApplicationController
+    skip_before_action :authorized, only: [:create]
     # CREATE 'login'
     def create
+        user = User.find_by(email: login_params[:email])
+        
+        if user.authenticate(login_params[:password])
+            @token = encode_token(user_id: user.id)
+            render json: { 
+                user: UserSerializer.new(user), 
+                jwt: @token 
+            }, 
+            status: :accepted
+        else
+            render json: {
+                error: 'failed to login',
+            }, status: :not_acceptable
+        end
+
     end
 
     # DESTROY 'logout'
     def destroy
     end
 
+    private
+
+    def login_params
+        params.require(:user).permit(:email, :password)
+    end
 
 end
