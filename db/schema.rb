0c61b80f2eb2ede6ad5ecbba7a3fd4eab12b2f6e
SVRourke <samuelvanrourke@gmail.com>
2021-03-15 08:49:40 -0400

    added password_digest and email to user, migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2021_03_12_021328) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
