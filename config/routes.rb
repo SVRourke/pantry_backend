5d25c88d89d417a82529d7752026ceecfd93425a
SVRourke <samuelvanrourke@gmail.com>
2021-03-16 14:40:45 -0400

    added items routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,11 @@ Rails.application.routes.draw do
     
   resources :lists, only: [] do
     resources :list_invites, only: [:create, :update, :destroy] 
+    resources :items, only: [:index, :show, :create, :update, :destroy]
   end  
 
   post '/search', to: 'users#search'
   get '/user_info', to: 'users#profile'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 # TODO: MOVE SIGN UP ACTION TO REGISTRATIONS CONTROLLER
