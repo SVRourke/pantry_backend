84c5a0f028c9c0a6d0eab9add0a6abdc2c40ef89
SVRourke <samuelvanrourke@gmail.com>
2021-03-16 14:05:45 -0400

    added friend index route

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,9 @@
 class FriendshipsController < ApplicationController
+    # DELETE /users/:user_id/friends/:id(.:format)        friendships#destroy
     # TODO: Index
+    def index
+        friends = current_user.friends
+        render json: friends, include: [:id, :name]
+    end
     # TODO: Delete
 end
