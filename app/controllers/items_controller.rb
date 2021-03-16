b4d999addd9bbae06d98d694211c68e4f35154e5
SVRourke <samuelvanrourke@gmail.com>
2021-03-16 14:40:32 -0400

    added create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,29 @@ class ItemsController < ApplicationController
         render json: items
     end
     # TODO: Create  POST   /lists/:list_id/items(.:format)              items#create
+    def create
+        item = List.find(params[:list_id]).items.build(item_params)
+        item.user = current_user
+        
+        if item.save
+            render json: {
+                message: "Saved Item"
+            }, status: :accepted
+        else
+            render json: {
+                message: "Error could not add #{item.name}"
+            }, status: :unprocessable_entity
+        end
+    end
     # TODO: Read    GET    /lists/:list_id/items/:id(.:format)          items#show
     # TODO: Update  PATCH  /lists/:list_id/items/:id(.:format)          items#update
     # TODO: Delete  DELETE /lists/:list_id/items/:id(.:format)          items#destroy
     
     # TODO: Mark Acquired
+
+    private
+    
+    def item_params
+        params.require(:item).permit(:name, :amount)
+    end
 end
