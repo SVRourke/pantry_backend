e38e6d596f6895f543b71f0928b1469b3f51ef92
SVRourke <samuelvanrourke@gmail.com>
2021-03-16 14:29:02 -0400

    added index action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,13 @@
 class ItemsController < ApplicationController
-    # TODO: Create
-    # TODO: Read
-    # TODO: Update
-    # TODO: Delete
+    # TODO: INDEX   GET    /lists/:list_id/items(.:format)              items#index
+    def index
+        items = Item.where(list_id: params[:list_id])
+        render json: items
+    end
+    # TODO: Create  POST   /lists/:list_id/items(.:format)              items#create
+    # TODO: Read    GET    /lists/:list_id/items/:id(.:format)          items#show
+    # TODO: Update  PATCH  /lists/:list_id/items/:id(.:format)          items#update
+    # TODO: Delete  DELETE /lists/:list_id/items/:id(.:format)          items#destroy
+    
     # TODO: Mark Acquired
 end
