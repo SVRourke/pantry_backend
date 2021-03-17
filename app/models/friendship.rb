6e5765d197b53a9a4f610ddd53734e44defb2b57
SVRourke <samuelvanrourke@gmail.com>
2021-03-17 11:21:07 -0400

    added custom validator

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,25 +1,28 @@
 # TODO: Validate friendship:unique
 
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend, class_name: 'User'
+    include ActiveModel::Validations
+    validates_with FriendshipValidator
 
-  after_create :reciprocate_friendship
-  after_destroy :remove_reciprocal
+    belongs_to :user
+    belongs_to :friend, class_name: 'User'
 
-  private
+    after_create :reciprocate_friendship
+    after_destroy :remove_reciprocal
 
-    def reciprocate_friendship
-        if !Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
-            Friendship.create(user_id: self.friend_id, friend_id: self.user_id)
+    private
+
+        def reciprocate_friendship
+            if !Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+                Friendship.create(user_id: self.friend_id, friend_id: self.user_id)
+            end
         end
-    end
 
-    def remove_reciprocal
-        opposite_friendship = Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
+        def remove_reciprocal
+            opposite_friendship = Friendship.find_by(user_id: self.friend_id, friend_id: self.user_id)
 
-        if opposite_friendship
-            opposite_friendship.destroy        
+            if opposite_friendship
+                opposite_friendship.destroy        
+            end
         end
-    end
 end
