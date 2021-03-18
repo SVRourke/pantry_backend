72bc14085585610dc0844ec3d4825e71cc039fe7
SVRourke <samuelvanrourke@gmail.com>
2021-03-18 14:33:25 -0400

    updated userserializer NOT FINISHED

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,15 @@
+# class FriendSerializer < ActiveModel::Serializer
+#   attributes :id, :name
+# end
+
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name
-
-  has_many :friends
-  has_many :pending_friends
-  has_many :requests, key: :requests
+  
+  has_many :friends, serializer: FriendsSerializer
+  has_many :pending_friends, serializer: FriendsSerializer
+  has_many :requests, key: :friend_requests, serializer: FriendRequestSerializer
   has_many :list_invites
   has_many :lists
-
-
+  
 end
 
