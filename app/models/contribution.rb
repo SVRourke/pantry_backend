a5c0575df319fc2c52235989ceab7896c5b42c5a
SVRourke <samuelvanrourke@gmail.com>
2021-03-18 08:50:38 -0400

    added after_destroy method to delete list if the last contributor leaves

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,6 +2,12 @@ class Contribution < ApplicationRecord
   include ActiveModel::Validations
   validates_with ContributionValidator
   
+  after_destroy :check_if_last
+
   belongs_to :user
   belongs_to :list
+
+  def check_if_last
+    self.list.destroy if !self.list.contributors.any?
+  end
 end
