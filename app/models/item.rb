046e6e77b33f08f5af80740a854866a569370f13
SVRourke <samuelvanrourke@gmail.com>
2021-03-18 07:47:26 -0400

    added validation for uniq and added messages

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 # ALERT: Update migration to set acquired default false
 
 class Item < ApplicationRecord
-  valideates :name, presence: true
+  valideates :name, presence: true, {message: "cannot add a blank item."}
+  validates :name, uniqueness: true{message: "#{value} already in list"}
 
   belongs_to :user
   belongs_to :list
