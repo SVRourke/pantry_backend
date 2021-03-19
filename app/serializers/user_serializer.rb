db2e95e9165866e895f059e081dce95eeed7e849
SVRourke <samuelvanrourke@gmail.com>
2021-03-19 08:39:08 -0400

    removed bulk references possilby slated for deletion

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,8 @@
 # class FriendSerializer < ActiveModel::Serializer
 #   attributes :id, :name
 # end
-
+# ? MAYBE DELETE ?
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name
-  
-  has_many :friends, serializer: FriendsSerializer
-  has_many :pending_friends, serializer: FriendsSerializer
-  has_many :requests, key: :friend_requests, serializer: FriendRequestSerializer
-  has_many :list_invites
-  has_many :lists
-  
 end
 
