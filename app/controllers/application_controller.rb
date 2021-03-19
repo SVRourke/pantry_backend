c4c80e26c86df4f50b27957267edfcfd720b81e4
SVRourke <samuelvanrourke@gmail.com>
2021-03-19 10:59:24 -0400

    added default rescues and helper methods to render various messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::API
     before_action :authorized 
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActionController::ParameterMissing, with: :model_errors
 
     def encode_token(payload)
         JWT.encode(
@@ -42,6 +45,58 @@ class ApplicationController < ActionController::API
         }, 
         status: :unauthorized unless logged_in?
     end
+
+
+    # Response Messages
+    def something_broke
+        render json: {
+            message: "Something broke, try again later..."},
+            status: :unprocessable_entity
+    end
+
+    def not_found()
+        render json: {
+            error: "Not Found"},
+            status: :not_found
+    end
+
+    def model_errors(error_messages)
+        render json: {
+            error: error_messages},
+            status: :unprocessable_entity
+    end
+
+    def unauthorized_message
+        render json: {
+            error: "Not Authorized"},
+            status: :unauthorized
+    end
+
+    def request_accepted
+        render json: {
+            message: "request accepted"},
+            status: :ok
+    end
+
+    def request_declined
+        render json: {
+            message: "request declined."},
+            status: :ok
+    end
+
+    def successful_destroy
+        render json: {
+            message: "record deleted!"},
+            status: :gone
+    end
+
+    def successful_create(record)
+        render json: {
+            message: "#{record.name} created #{record.created_at}"}, 
+            status: :created
+    end
+
+
 end
 
 
