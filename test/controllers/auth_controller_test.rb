54e3cb5326e86c50f5ecf59f5d05d756ca566448
SVRourke <samuelvanrourke@gmail.com>
2021-03-21 12:23:10 -0400

    added tests

--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bad_login = { 
+      user: { 
+        email: "geralt@rivia.com", 
+        password: "yenniffer" 
+      } 
+    }
+    @good_login = {
+      user: {
+        email: 'good@login.com',
+        name: 'good',
+        password: 'password'
+      }
+    }
+    User.create(@good_login[:user])
+  end
+
+
+  test "does not issue a JWT with incorrect login" do
+    post login_path, params: @bad_login, as: :json
+    assert_response :unauthorized
+  end
+  
+  test "response ok with proper credentials" do
+    post login_path, params: @good_login, as: :json
+    assert_response :created, "Wrong response Code"
+  end
+  
+  test "issue a JWT with proper credentials" do
+    post login_path, params: @good_login, as: :json
+    body = JSON.parse(@response.body)
+    assert body['jwt'], "No JWT ISSUED"
+  end
+
+  # invalidates a JWT on destroy
 end
