cff13aa65d9bbb15bdaf53be0ecdff3b76c4a6b7
SVRourke <samuelvanrourke@gmail.com>
2021-03-24 08:10:22 -0400

    created friends serializer

--- app/serializers/friends_serializer.rb
+++ app/serializers/friends_serializer.rb
@@ -1,3 +1,9 @@
 class FriendsSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :friendship_age
+
+  def friendship_age
+    fs = Friendship.where(user_id: current_user.id, friend_id: object)
+    # byebug
+    fs[0].record_age()
+  end
 end
