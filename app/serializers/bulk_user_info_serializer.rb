4f16272546274e9209909a4d1ef6cd9a2a4d97a5
SVRourke <samuelvanrourke@gmail.com>
2021-03-24 17:55:17 -0400

    updated

--- app/serializers/bulk_user_info_serializer.rb
+++ app/serializers/bulk_user_info_serializer.rb
@@ -1,10 +1,19 @@
 class BulkUserInfoSerializer < ActiveModel::Serializer
   attributes :id, :email, :name
-  
+  # attribute: :friend_requests, each_serializer: :FriendRequestSerializer
+  attribute: list_requests: {each_serializer: MinimalSerializer}
+
+  def friend_requests
+    ActiveModel::Serializer::CollectionSerializer.new(object.all_requests.to_a, {each_serializer: FriendRequestSerializer})
+  end
     
-  has_many :friends, serializer: MinimalSerializer
-  has_many :pending_friends, serializer: FriendsSerializer
-  has_many :requests, key: :friend_requests, serializer: FriendRequestSerializer
-  has_many :list_invites
+  def list_requests
+    object.all_list_invites
+  end
+  
+  has_many :friendships, key: :friends, serializer: FriendsSerializer
+  # has_many :pending_friends, serializer: MinimalSerializer
+  # has_many :requests, key: :friend_requests, serializer: FriendRequestSerializer
+  # has_many :list_invites
   has_many :lists, serializer: MinimalSerializer
 end
