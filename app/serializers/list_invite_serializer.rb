1dd9c3ce9866407463a964e16a4f167be418907b
SVRourke <samuelvanrourke@gmail.com>
2021-03-25 14:47:25 -0400

    created list invite serializer

---
+++ app/serializers/list_invite_serializer.rb
@@ -0,0 +1,20 @@
+class ListInviteSerializer < ActiveModel::Serializer
+  attributes :id, :record_age, :requestor_name, :requestee_name, :contributor_count, :type
+
+  def requestor_name
+    object.requestor.name
+  end
+
+  def requestee_name
+    object.pending_contributor.name
+  end
+
+  def contributor_count
+    object.list.contributors.count
+  end
+
+  def type
+    object.requestor == current_user ? "sent" : "received";
+  end
+
+end
