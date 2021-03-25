8fb2b7441d712217de759e809da76bc0df3646de
SVRourke <samuelvanrourke@gmail.com>
2021-03-25 14:49:52 -0400

    added attribute, removed commented out method

--- app/serializers/friends_serializer.rb
+++ app/serializers/friends_serializer.rb
@@ -1,13 +1,8 @@
 class FriendsSerializer < ActiveModel::Serializer
-  attributes :id, :friend_name, :record_age
+  attributes :id, :friend_name, :record_age, :friend_id 
   
   def friend_name
     object.friend.name
   end
 
-  # def friendship_age
-  #   byebug
-  #   fs = Friendship.where(user_id: current_user.id, friend_id: object)
-  #   fs[0].record_age()
-  # end
 end
