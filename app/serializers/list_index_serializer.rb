6ca22e24ad57650be228ff33645fa13c4da2e47b
SVRourke <samuelvanrourke@gmail.com>
2021-03-25 14:47:43 -0400

    added item count for list index serializer

--- app/serializers/list_index_serializer.rb
+++ app/serializers/list_index_serializer.rb
@@ -1,7 +1,11 @@
 class ListIndexSerializer < ActiveModel::Serializer
-  attributes :id, :name, :contributor_count
+  attributes :id, :name, :contributor_count, :item_count
   
   def contributor_count
     object.contributors.count
+  end
+
+  def item_count
+    object.items.count
   end
 end
