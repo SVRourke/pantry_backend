a3ff73a731c229d4b351bede77a05bac77105a3b
SVRourke <samuelvanrourke@gmail.com>
2021-03-26 08:16:46 -0400

    refactored

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,27 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest  
+  
+  def self.prepare
+    post users_path, params: {
+      user: {
+        email: 'gooder@login.com',
+        name: 'good',
+        password: 'password'}
+      }, 
+      as: :json
+
+      @user_params_1 = {
+        user: {
+          email: '1234@login.com',
+          name: 'good',
+          password: 'password'
+          }
+        }
+
+
+    @auth_header = "Bearer #{JSON.parse(@response.body)['JWY']}"
+  end
 
   test 'does not create an invalid user' do
     post users_path, params: { 
@@ -49,57 +70,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
   end
 
-  # TODO: CHECK RESPONSE BODY
-  test 'returns full user info' do
-    params = {
-      user: {
-        email: 'goodest@login.com',
-        name: 'good',
-        password: 'password'}}
-
-    post users_path, params: params, as: :json
-
-    body = JSON.parse(@response.body)
-
-  end
-
   test 'show is protected by auth' do
     get user_path(id: 600)
     assert_response :unauthorized
   end
 
   test 'show will show the logged in user' do
-    post users_path, params: {
-      user: {
-        email: 'gooder@login.com',
-        name: 'good',
-        password: 'password'}
-      }, as: :json
-
+    post users_path, params: @user_params_1, as: :json
+    
     body = JSON.parse(@response.body)
 
     get user_path(id: body['user']['id']), 
-      headers: { 
-        Authorization: "Bearer #{body['jwt']}"}
-    assert_response :ok
+      headers: { Authorization: "Bearer #{body['jwt']}" }
+    
+      assert_response :ok
   end
 
   test 'show will only show the logged in user' do
+    user = User.create(email: "s@s.com", name: "s", password: "password")
 
-    post users_path, params: {
-      user: {
-        email: 'gooder@login.com',
-        name: 'good',
-        password: 'password'}
-      }, 
-      as: :json
-
-    body = JSON.parse(@response.body)
-    other = User.create(email: "s@s.com", name: "s", password: "password")
-
-    get user_path(id: other.id), 
-      headers: { 
-        Authorization: "Bearer #{body['jwt']}"}
+    get user_path(id: 600), 
+      headers: { Authorization: @auth_header }
 
     assert_response :unauthorized
   end
