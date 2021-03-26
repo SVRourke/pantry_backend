0df65aacffb09d118ebbc6c6d327b13c19094b7b
SVRourke <samuelvanrourke@gmail.com>
2021-03-26 08:16:28 -0400

    began stubbing out list controller test

--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ListsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def self.prepare
+    # create a list add contributors and items
+    @list = List.create(name: "Testing List")
+    @list.contributors.push(User.create(name: "test1", email: "123@123.com", password: "password"))
+    @list.contributors.push(User.create(name: "test2", email: "4556@456.com", password: "password"))
+    byebug
+  end
+
+  def teardown
+    @list.contributors.destroy_all
+    @list.destroy
+  end
+
+  test 'show returns a list' do 
+    get list_path(@list.id)
+  end
+  # show returns a list's contributors
+  # show returns a lis's items
 end
