8b0b6cf46c637589af22ecd6cc0cadf5dd0ce0ca
SVRourke <samuelvanrourke@gmail.com>
2021-04-09 09:30:28 -0400

    added mutual_list_count

--- app/serializers/friends_serializer.rb
+++ app/serializers/friends_serializer.rb
@@ -1,5 +1,5 @@
 class FriendsSerializer < ActiveModel::Serializer
-  attributes :id, :friend_name, :record_age, :friend_id 
+  attributes :id, :friend_name, :record_age, :friend_id, :mutual_list_count 
   
   def friend_name
     object.friend.name
