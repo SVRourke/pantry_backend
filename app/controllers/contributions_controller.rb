574ac0d25c83e0ba444f5b82cece3357358a31c9
SVRourke <samuelvanrourke@gmail.com>
2021-04-20 13:26:37 -0400

    added index action for contributions

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,4 +1,8 @@
 class ContributionsController < ApplicationController
     # TODO: Index
+    def index
+        list = List.find(params[:list_id])
+        render json: list.contributions, status: :ok
+    end
     # TODO: Destroy
 end
