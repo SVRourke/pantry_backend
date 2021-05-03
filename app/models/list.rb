84beb34f71660a21bdfd44d023b5e5ef806430c2
SVRourke <samuelvanrourke@gmail.com>
2021-05-03 10:18:21 -0400

    added cascading destroy

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,12 +1,12 @@
 # TODO: add logic to delete list no contributors left
 
 class List < ApplicationRecord
-    has_many :contributions, foreign_key: :list_id
+    has_many :contributions, foreign_key: :list_id, dependent: :destroy
     has_many :contributors, through: :contributions, source: :user
 
-    has_many :items
+    has_many :items, dependent: :destroy
 
-    has_many :list_invites
+    has_many :list_invites, dependent: :destroy
     has_many :invited_users, through: :list_invites, source: :pending_contributor 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,20 +10,20 @@ class User < ApplicationRecord
     has_many :friends, through: :friendships
 
     # sent friend requests
-    has_many :sent_requests, foreign_key: :requestor_id, class_name: 'Friendrequest'
+    has_many :sent_requests, foreign_key: :requestor_id, class_name: 'Friendrequest', dependent: :delete_all
     # requested users, new request created by pushing user onto this list
     has_many :pending_friends, through: :sent_requests
     
     # recieved friend requests 
-    has_many :requests, foreign_key: :pending_friend_id, class_name: 'Friendrequest'
+    has_many :requests, foreign_key: :pending_friend_id, class_name: 'Friendrequest', dependent: :delete_all
     # users requesting friendship
     has_many :requestors, through: :requests
 
     # recieved list_invites
-    has_many :list_invites, foreign_key: :pending_contributor_id, class_name: 'ListInvite'
-    has_many :sent_list_invites, foreign_key: :requestor_id, class_name: 'ListInvite'
+    has_many :list_invites, foreign_key: :pending_contributor_id, class_name: 'ListInvite', dependent: :destroy
+    has_many :sent_list_invites, foreign_key: :requestor_id, class_name: 'ListInvite', dependent: :destroy
 
-    has_many :contributions
+    has_many :contributions, dependent: :destroy
     has_many :lists, through: :contributions, source: :list 
 
     def leave_list(list)
