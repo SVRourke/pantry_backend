5ea4457f759241e8091049b1d3fe4f25fccfca80
SVRourke <samuelvanrourke@gmail.com>
2021-05-05 11:29:17 -0400

    modified for jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
 class UsersController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: :create
-    skip_before_action :authorized, only: [:create]
+    skip_before_action :authenticate, only: :create
+    serialization_scope :view_context
 
     def create
-        user = User.create(
+        user = User.new(
             name: user_params[:name],
             email: user_params[:email].upcase,
             password: user_params[:password]
         )
 
-        if user.valid?
-            bake_cookies(user.id)
-            render json: user, status: :created
+        if user.save
+            render json: {
+                jwt: build_jwt(user.id), 
+                id: user.id},
+                status: :created
             return
         end
-
         model_errors(user.errors.full_messages)
+
     end
 
     def show
@@ -27,8 +29,6 @@ class UsersController < ApplicationController
 
     def destroy
         current_user.destroy
-        cookies.delete :id
-        cookies.delete :'CSRF-TOKEN'
         successful_destroy()
     end
     
