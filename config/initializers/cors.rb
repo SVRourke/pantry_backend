29a1be9dbf512b9399753574e271a6cbfa6f51f3
SVRourke <samuelvanrourke@gmail.com>
2021-05-06 07:34:28 -0400

    updated the origin url for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins 'https://pantry.svrourke.com'
     resource('*', headers: :any, methods: :any, credentials: true)
     end
 end
