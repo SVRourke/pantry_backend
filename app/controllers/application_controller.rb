de15e4fc56345e444b05eea4412286910e12b3f3
SVRourke <samuelvanrourke@gmail.com>
2021-05-20 08:25:28 -0400

    reworked for cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,61 +1,42 @@
 class ApplicationController < ActionController::API
-    before_action :authenticate
+    before_action :authorized
+    
+    include ActionController::Cookies
+    include ActionController::RequestForgeryProtection
 
-    rescue_from 'JWT::ExpiredSignature' do |exception|
-        render json: {
-            error: "Please Log In"},
-            status: 401
+    protect_from_forgery with: :exception
+    before_action :set_csrf_cookie
+
+    def bake_cookie(id)
+        cookies.signed[:id] = {
+            value: id,
+            httponly: true,
+            expires: 1.day.from_now
+        }
+    end
+
+    def current_user
+        current_user = current_user ||= User.find(session[:user_id])
     end
 
     def logged_in?
         !!current_user
     end
 
-    def current_user
-        if auth_present?
-            user = User.find(decoded_token['id'])
-
-            if user
-                @current_user ||= user
-            end
-        end
+    def authorized
+        render json: { 
+            message: 'Please log in' 
+        }, 
+        status: :unauthorized unless logged_in?
     end
-
-    def authenticate
-        unless logged_in?
-            render json: {error: 'unauthorized'}, status: 401
-        end
-    end
-
-    def build_jwt(id)
-        payload = {
-            'iss': 'pantry-api',
-            'exp': 1.hour.from_now.to_i,
-            'id': id
-        }
-        return JWT.encode payload, ENV["JWT_KEY"], 'HS256'
-    end
-
-  
 
     private
 
-    def token
-        request.headers['Authorization']
-    end
-    
-    def decoded_token
-        begin
-            t = JWT.decode token(), ENV["JWT_KEY"], true, { algorithm: 'HS256' }
-            t[0]
-        rescue
-            return false
-        end
+    def set_csrf_cookie
+        cookies["CSRF-TOKEN"] = form_authenticity_token
     end
 
-    def auth_present?
-        !!decoded_token()
-    end
-    
 end
+
+    
 
