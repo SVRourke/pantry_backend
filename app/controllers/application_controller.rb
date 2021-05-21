ed1e5e7e6e48c037a9879eb476359fb69ffe95ba
SVRourke <samuelvanrourke@gmail.com>
2021-05-21 07:05:00 -0400

    after action? set csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :exception
-    before_action :set_csrf_cookie
+    after_action :set_csrf_cookie
 
     def bake_cookie(id)
         cookies.signed[:id] = {
